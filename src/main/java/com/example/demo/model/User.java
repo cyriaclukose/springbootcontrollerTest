83e9508358486a81cf2b37bd9323272a16bed8cb
cyriaclukose <cyriaclukosekuruvila@gmail.com>
2020-12-01 20:29:32 +0530

    updating user model

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,15 +1,5 @@
 package com.example.demo.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
 public class User {
 	
 	private int id;
